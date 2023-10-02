f9ef5520cd5c3f5cc23741e9d81de06fe81785d1
Eduardo Yutaka Nakanishi <eduardo.nakanishi@gmail.com>
2023-10-02 16:02:43 -0300

    feat: create index and show article pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/articles", to: "articles#index"
+  root "articles#index"
+
+  resources :articles
 end
